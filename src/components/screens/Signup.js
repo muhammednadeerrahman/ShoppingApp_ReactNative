f9777aaa4a102bf40db49f4d7e09408bde6b16b1
muhammednadeerrahman <mohdnadeerrahman@gmail.com>
2023-12-16 17:42:03 +0530

    firstcommit

---
+++ src/components/screens/Signup.js
@@ -0,0 +1,12 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function signup() {
+  return (
+    <View>
+        <Image source={require("../../assets/images/signup.png")} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
